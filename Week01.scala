675115d59e42d8d947843e011fe78237b8b37531
gamzeakyol <gamze.akyol.3@gmail.com>
2018-07-27 14:59:18 +0300

    Update Week01.scala

--- Week01.scala
+++ Week01.scala
@@ -1,8 +1,8 @@
 //1.soru
 def max(xs: List[Int]): Option[Int] = xs match {
-  case Nil => None
-  case List(x: Int) => Some(x)
-  case x :: y :: tail => max((if (x > y) x else y) :: tail)
+	case Nil => None
+  	case List(x: Int) => Some(x)
+  	case x :: y :: tail => max((if (x > y) x else y) :: tail)
 }
 
 //2.soru
