84cd2460dd1f6850d8c7925d912cf538de694493
gamzeakyol <gamze.akyol.3@gmail.com>
2018-07-27 14:52:36 +0300

    Update Week01.scala

--- Week01.scala
+++ Week01.scala
@@ -1,6 +1,12 @@
-
+//1.soru
 def max(xs: List[Int]): Option[Int] = xs match {
   case Nil => None
   case List(x: Int) => Some(x)
-  case x :: y :: tail => max( (if (x > y) x else y) :: tail )
+  case x :: y :: tail => max((if (x > y) x else y) :: tail)
+}
+
+//2.soru
+def diff(xs: List[Int]): Int = {
+	val difference = xs.max - xs.min
+	return difference 
 }
